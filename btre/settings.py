3fc983bafc359a1914d9d8725d17fa74b75e3ae5
Ervin Szekely <ervin.szekely88@gmail.com>
2019-05-05 12:15:47 +0200

    Local Settings File

--- btre/settings.py
+++ btre/settings.py
@@ -143,3 +143,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ervin.szekely@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
